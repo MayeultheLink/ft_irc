258cc21b8f40d3620ef1d69a4c8219a1cf75b6b0
Mayeul DE LA SERRE <mde-la-s@e2r8p19.clusters.42paris.fr>
2023-04-27 12:55:46 +0200

    irc

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "Server.hpp"
+#include <iostream>
+#include <stdexcept>
+
+int main(int ac, char** av) {
+	try {
+		if (ac != 3)
+			throw std::runtime_error("Usage: ./ircserver <port> <password>");
+		Server server(av[1], av[2]);
+		server.launch();
+		return 0;
+	}
+	catch (const std::exception & ex) {
+		std::cerr << ex.what() << std::endl;
+		return 1;
+	}
+}
